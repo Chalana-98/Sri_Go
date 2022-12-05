2f0cd72804da7dec8d416dc975087a9b0ce60ad0
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2022-12-05 16:00:44 +0530

    Start

---
+++ lib/Login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Login_page extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'dart:async';
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sri_go/Login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  SplashScreenState createState()=> SplashScreenState();
+}
+class SplashScreenState extends State<MyHomePage> {
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 5),
+        ()=>Navigator.pushReplacement(context,MaterialPageRoute(builder:
+    (context)=>HomeScreen()
+      )
+     )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+     color: Colors.white70,
+      child: Image.asset('assets/images/launch_image.png',
+        width: 200.00,
+        height: 200.00,
+
+      )
+   );
+
+  }
+  
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: (){
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => Login_page()
+                ));
+          },
+          //icon: Icon(Icons.ac_unit_sharp),
+          label: Text("ok"),
+          ),
+
+
+     // appBar: AppBar(title: Text("Sri_Go")),
+      body: ListView(
+        children: [
+
+          CarouselSlider(
+
+              items: [
+                Container(
+                  margin: EdgeInsets.all(3.0),
+                  decoration: BoxDecoration(
+
+                    borderRadius: BorderRadius.circular(6.0),
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/photo_2.jpg'),
+                      fit: BoxFit.contain,
+                    ),
+
+                  ),
+
+
+                ),
+
+                Container(
+                  margin: EdgeInsets.all(6.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6.0),
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/photo_1.jpg'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.all(6.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6.0),
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/photo_3.jpg'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+
+              ],
+              options: CarouselOptions(
+                  height: 300.0,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 16 / 9,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  viewportFraction: 0.8
+
+              )
+
+          ),
+
+          Column(
+            children: [
+
+              Text('You can find the Destination you are interested in, find Accommodation facilities as well as Transport facilities to reach there easily through   Sri Go.',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.openSans(
+                    fontSize: 30
+                ),
+              )
+
+            ],
+          ),
+
+
+        ],
+
+      ),
+
+
+    );
+
+
+
+
+  }
+  
+}
+
+
+
