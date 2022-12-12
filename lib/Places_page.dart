d2c1d5267b129a63cc952819c098e5383fac59a2
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2022-12-13 01:55:08 +0530

    day 06 FINISH

--- lib/Places_page.dart
+++ lib/Places_page.dart
@@ -11,6 +11,12 @@ class Place_page extends StatefulWidget {
 class _Place_pageState extends State<Place_page> {
   @override
   Widget build(BuildContext context) {
+
+    var arrDistrics = ['Ampara',"Anuradhapura,",'Badulla','Batticaloa',
+                         'Colombo','Galle','Gampaha','Hambantota','Jaffna','Kalutara',
+                       'Kandy','Kegalle','Kilinochchi','Kurunegala','Mannar','Matale',
+                      'Matara','Monaragala','Mullaitivu','Nuwara Eliya','Polonnaruwa',
+               'Puttalam','Ratnapura','Trincomalee','Vavuniya'];
     return Scaffold(
       appBar: AppBar(
         title: Text('Place'),
@@ -21,7 +27,11 @@ class _Place_pageState extends State<Place_page> {
       ),
 
       body:  Center(
+
+
         child: Container(
+          padding: EdgeInsets.all(8),
+
           decoration: BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topRight,
@@ -34,7 +44,36 @@ class _Place_pageState extends State<Place_page> {
 
             )
           ),
+
+          child: new ListView.separated(itemBuilder: (context, index) {
+            return Container(
+               height: 100,
+
+
+               decoration: BoxDecoration(
+                 borderRadius: BorderRadius.circular(20),
+
+                   gradient: LinearGradient(
+                       begin: Alignment.topRight,
+                       end: Alignment.bottomLeft,
+                       colors: [
+                         Colors.black87,
+                         Colors.white
+
+                       ]
+
+                   )
+               ),
+                child: Text(arrDistrics[index]));
+          },
+            itemCount: arrDistrics.length,
+            separatorBuilder: ( context, int index) {
+            return Divider(height: 5,thickness: 5,);
+            },
+
+          )
         ),
+
       ),
     );
   }
